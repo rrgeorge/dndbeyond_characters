0ef13edf65aa0725279edd30c9ec705544f90869
Robert George <rgeorge@midnightweb.net>
2019-07-10 18:20:01 -0700

    Initial Commit

---
+++ D&D Beyond Characters/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  D&D Beyond Characters
+//
+//  Created by Robert George on 7/10/19.
+//  Copyright © 2019 Robert George. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ D&D Beyond CharactersTests/D_D_Beyond_CharactersTests.swift
@@ -0,0 +1,34 @@
+//
+//  D_D_Beyond_CharactersTests.swift
+//  D&D Beyond CharactersTests
+//
+//  Created by Robert George on 7/10/19.
+//  Copyright © 2019 Robert George. All rights reserved.
+//
+
+import XCTest
+@testable import D_D_Beyond_Characters
+
+class D_D_Beyond_CharactersTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
