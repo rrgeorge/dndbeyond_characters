405a42fa903f855e19b46dc733f0f72696a5242e
Robert George <rgeorge@midnightweb.net>
2020-07-20 18:14:10 -0700

    removed bad test code

--- D&D Beyond Characters/ViewController.swift
+++ D&D Beyond Characters/ViewController.swift
@@ -944,35 +944,29 @@ class ViewController: UIViewController, WKUIDelegate, UIActionSheetDelegate, UIG
             var request = URLRequest(url: url)
             request.httpMethod = "POST"
             do {
-                request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted) // pass dictionary to data object and set it as request body
+                request.httpBody = try JSONSerialization.data(withJSONObject: data, options: .prettyPrinted) // pass dictionary to data object and set it as request body
             } catch let error {
                 print(error.localizedDescription)
-                completion(nil, error)
             }
             request.addValue("application/json", forHTTPHeaderField: "Content-Type")
             request.addValue("application/json", forHTTPHeaderField: "Accept")
             let task = session.dataTask(with: request, completionHandler: { data, response, error in
                 guard error == nil else {
-                    completion(nil, error)
                     return
                 }
 
                 guard let data = data else {
-                    completion(nil, NSError(domain: "dataNilError", code: -100001, userInfo: nil))
                     return
                 }
 
                 do {
                     //create json object from data
                     guard let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] else {
-                        completion(nil, NSError(domain: "invalidJSONTypeError", code: -100009, userInfo: nil))
                         return
                     }
                     print(json)
-                    completion(json, nil)
                 } catch let error {
                     print(error.localizedDescription)
-                    completion(nil, error)
                 }
             })
             task.resume()
